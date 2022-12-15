e01dfbdcb09d730209246b893ceb58b1b7379312
KaenBin <113847693+KaenBin@users.noreply.github.com>
2022-12-15 22:11:42 +0700

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -1,4 +1,80 @@
 USE `outmeta`;
+
+--View get detail follower
+DROP procedure IF EXISTS `get_follower`;
+DELIMITER $$
+CREATE PROCEDURE `get_follower`(user_id VARCHAR(25))
+BEGIN
+    SELECT 
+        `user`.user_id,
+        password,
+        description,
+        name,
+        address,
+        dob,
+        phone_number,
+        email,
+        follower_count
+    FROM `user` JOIN `user_follower`
+    ON `user`.user_id = `user_follower`.follower_id
+
+    WHERE `user_follower`.user_id = user_id;
+END$$
+DELIMITER;
+
+--View get detail users that given user_id is following
+DROP procedure IF EXISTS `get_following`;
+DELIMITER $$
+CREATE PROCEDURE `get_following`(user_id VARCHAR(25))
+BEGIN
+    SELECT 
+        `user`.user_id,
+        password,
+        description,
+        name,
+        address,
+        dob,
+        phone_number,
+        email,
+        follower_count
+    FROM `user` JOIN `user_follower`
+    ON `user`.user_id = `user_follower`.user_id
+
+    WHERE `user_follower`.follower_id = user_id;
+END$$
+DELIMITER;
+
+--View detail topics that a user is following
+
+DROP procedure IF EXISTS `get_topics`;
+
+DELIMITER $$
+CREATE PROCEDURE `get_topics`(user_id VARCHAR(25))
+BEGIN
+        SELECT 
+		`topic_follower`.follower_id as user_id,
+        `topic`.topic_id,
+        `topic`.topic_title,
+        `topic`.follower_count
+    FROM outmeta.`topic` 
+    JOIN `topic_follower`
+    ON `topic`.topic_id = `topic_follower`.topic_id
+
+    WHERE `topic_follower`.follower_id = user_id;
+END$$
+DELIMITER;
+
+DROP procedure IF EXISTS `view_group_user`;
+DELIMITER $$
+CREATE PROCEDURE `view_group_user`(user_id VARCHAR(25))
+BEGIN
+	SELECT *
+    FROM `group`, group_member
+    WHERE 	`group`.group_id = group_member.group_id AND
+			group_member.member_id = user_id;
+END$$
+DELIMITER;
+
 DROP PROCEDURE IF EXISTS `view_post_group`;
 DELIMITER $$
 CREATE PROCEDURE `view_post_group`(group_id INT)
