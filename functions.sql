5a033da3f17bd189b2f6491a882e3a6c85cc7702
KaenBin <loi.phan1342002@hcmut.edu.vn>
2022-12-15 23:16:52 +0700

    comment functions

--- functions.sql
+++ functions.sql
@@ -1,24 +1,34 @@
 USE `outmeta`;
 
+-- function to write a new post from a user
+-- INPUT = new content, new type of media, author id
+-- OUTPUT = TRUE
 DROP function IF EXISTS `add_post`;
 DELIMITER $$
-CREATE FUNCTION `add_post`(add_post_id INT, add_post_content VARCHAR(1000), add_post_media BLOB, add_writer_id VARCHAR(25)) RETURNS tinyint(1)
+CREATE FUNCTION `add_post`(add_post_content VARCHAR(1000), add_post_media BLOB, add_writer_id VARCHAR(25)) RETURNS tinyint(1)
     DETERMINISTIC
 BEGIN
-	INSERT INTO post (post_id, post_datetime, post_content, post_media, writer_id)
-    VALUES (add_post_id, current_timestamp(), add_post_content, add_post_media, add_writer_id);
+	INSERT INTO post (post_datetime, post_content, post_media, writer_id)
+    VALUES (current_timestamp(), add_post_content, add_post_media, add_writer_id);
 RETURN TRUE;
 END$$
-DELIMITER;
+DELIMITER ;
 
+-- function to edit existing post
+-- INPUT = new post id, new content, new type of media
+-- OUTPUT = TRUE if successfully edit post, FALSE if post is not found
 DROP function IF EXISTS `edit_post`;
 DELIMITER $$
 CREATE FUNCTION `edit_post`(edit_post_id INT, edit_post_content VARCHAR(1000), edit_post_media BLOB) RETURNS tinyint(1)
     DETERMINISTIC
 BEGIN
-	IF (SELECT * FROM post WHERE post.post_id = edit_post_id)
+	IF (SELECT COUNT(*) FROM post WHERE post.post_id = edit_post_id)
     THEN
-
+		UPDATE post
+        SET post_content = edit_post_content,
+			post_media = edit_post_media
+		WHERE 
+			post_id = edit_post_id;
 		RETURN TRUE;
     ELSE
 		SIGNAL SQLSTATE '45000'
@@ -26,29 +36,34 @@ BEGIN
 		RETURN FALSE;
 	END IF;
 END$$
-DELIMITER;
+DELIMITER ;
 
+-- function to change mangager of an existing group
+-- INPUT = manager id, group id
+-- OUTPUT = TRUE IF successfully change manager of a group, FALSE if group not exist
 DROP function IF EXISTS `change_manager`;
 DELIMITER $$
-CREATE FUNCTION `change_manager`(user_id varchar(25), group_manager_id INT) RETURNS tinyint(1)
+CREATE FUNCTION `change_manager`(user_id varchar(25), group_id INT) RETURNS tinyint(1)
     DETERMINISTIC
 BEGIN
-	IF (SELECT COUNT(*) FROM `group` WHERE `group`.group_id = group_manager_id)
+	IF (SELECT COUNT(*) FROM `group` WHERE `group`.group_id = group_id)
     THEN
 		UPDATE `group`
 		SET
 			`group`.manager_id = user_id
 		WHERE
-			`group`.group_id = group_manager_id;
+			`group`.group_id = group_id;
 		RETURN TRUE;
     ELSE
 		SIGNAL SQLSTATE '45000'
 			SET MESSAGE_TEXT = 'Group not found in group table';
 	END IF;
 END$$
-DELIMITER;
+DELIMITER ;
 
-/* Add new user */
+-- function to add a new user
+-- INPUT = new user id, new password, new description, new name, new address, new day of birth, new phone number, new email
+-- OUTPUT = "SUCCESSFUL" IF successfully add a new user, "DUPLICATED" IF the user id is already exist
 DROP function IF EXISTS `add_user`;
 DELIMITER $$
 CREATE FUNCTION `add_user`(
@@ -65,21 +80,23 @@ RETURNS varchar(20) DETERMINISTIC
    
 BEGIN
 	IF(SELECT COUNT(*) FROM `user` WHERE `user`.user_id = new_user_id) /* finds if new_user_id is unique or not */
-    THEN --TRUE
+    THEN -- TRUE
 		RETURN "DUPLICATED user_id";
-	ELSE --FALSE
+	ELSE -- FALSE
 		INSERT INTO `user` (user_id, password, description, name, address, dob, phone_number, email) 
         VALUES 
         (new_user_id, new_password, new_description, new_name, new_address, new_dob, new_phoneno, new_email);
         RETURN "SUCCESSFUL";
 	END IF;
 END$$
-DELIMITER;
+DELIMITER ;
 
-/* Change_user_info */
+-- function to change user info
+-- INPUT = new user id, new password, new description, new name, new address, new day of birth, new phone number, new email
+-- OUTPUT = "DATA MODIFIED SUCCESSFULLY" IF successfully change the user info, "USER NOT FOUND" IF the user id is not found
 DROP function IF EXISTS `change_user_info`;
 DELIMITER $$
-CREATE FUNCTION change_user_info ( --modifies infomation of the given userid
+CREATE FUNCTION change_user_info ( -- modifies infomation of the given userid
 	userid VARCHAR(25),
 	new_password VARCHAR(16), 
 	new_description VARCHAR(200), 
@@ -92,7 +109,7 @@ CREATE FUNCTION change_user_info ( --modifies infomation of the given userid
 RETURNS VARCHAR(30) DETERMINISTIC
 BEGIN
 	IF (SELECT COUNT(*) FROM `user` where `user`.user_id = userid) /* finds userid exists in the database */
-	THEN --TRUE
+	THEN -- TRUE
 	/* update password */
 		UPDATE `user`
 		SET `user`.password = new_password
@@ -129,8 +146,8 @@ BEGIN
 		WHERE `user`.user_id = userid;
     
 		RETURN "DATA MODIFIED SUCCESSFULLY";
-	ELSE --FALSE
+	ELSE -- FALSE
 		RETURN "USER NOT FOUND";
 END IF;
 END$$
-DELIMITER;
+DELIMITER ;
