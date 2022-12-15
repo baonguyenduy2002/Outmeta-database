c2b5fe88494819fdefbeab3f537846301514c195
HoangTran <jonny.tran12902@gmail.com>
2022-12-16 00:59:09 +0700

    comment on trigger

--- trigger.sql
+++ trigger.sql
@@ -2,6 +2,9 @@
 --Add trigger for tables
 --
 
+-- Trigger increase reply_count by 1 in post_comment table whenever insert new rercord to comment_reply table
+-- INPUT: AFTER INSERT ON `comment_reply`
+-- OUTPUT: post_comment.reply_count = post_comment.reply_count + 1
 DELIMITER $$
 CREATE TRIGGER `comment_reply_AFTER_INSERT` 
 AFTER INSERT ON `comment_reply` 
@@ -11,6 +14,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger decrease reply_count by 1 in post_comment table whenever delete a rercord from comment_reply table
+-- INPUT: AFTER DELETE ON `comment_reply`
+-- OUTPUT: post_comment.reply_count = post_comment.reply_count - 1
 DELIMITER $$
 CREATE TRIGGER `comment_reply_AFTER_DELETE` 
 AFTER DELETE ON `comment_reply` 
@@ -20,6 +27,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger increase react_count by 1 in comment table whenever insert new rercord to comment_user_reaction table
+-- INPUT: AFTER INSERT ON `comment_user_reaction`
+-- OUTPUT: comment.react_count = comment.react_count + 1
 DELIMITER ;;
 CREATE TRIGGER `comment_user_reaction_AFTER_INSERT` 
 AFTER INSERT ON `comment_user_reaction` 
@@ -29,6 +40,10 @@ BEGIN
 END ;;
 DELIMITER ;
 
+
+-- Trigger decrease react_count by 1 in comment table whenever delete a rercord from comment_user_reaction table
+-- INPUT: AFTER DELETE ON `comment_user_reaction`
+-- OUTPUT: comment.react_count = comment.react_count - 1
 DELIMITER $$
 CREATE TRIGGER `comment_user_reaction_AFTER_DELETE` 
 AFTER DELETE ON `comment_user_reaction` 
@@ -38,6 +53,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger increase member_count by 1 in group table whenever insert new rercord to group_member table
+-- INPUT: AFTER INSERT ON `group_member`
+-- OUTPUT: group.member_count = group.member_count + 1
 DELIMITER $$
 CREATE TRIGGER `group_member_AFTER_INSERT` 
 AFTER INSERT ON `group_member` 
@@ -47,6 +66,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger decrease member_count by 1 in group table whenever delete a rercord from group_member table
+-- INPUT: AFTER DELETE ON `group_member`
+-- OUTPUT: group.member_count = group.member_count - 1
 DELIMITER $$
 CREATE TRIGGER `group_member_AFTER_DELETE` 
 AFTER DELETE ON `group_member` 
@@ -56,6 +79,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger increase comment_count by 1 in post table whenever insert new rercord to post_comment table
+-- INPUT: AFTER INSERT ON `post_comment`
+-- OUTPUT: post.comment_count = post.comment_count + 1
 DELIMITER $$
 CREATE TRIGGER `post_comment_AFTER_INSERT` 
 AFTER INSERT ON `post_comment` 
@@ -65,6 +92,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger update comment_count in post table whenever update a rercord in post_comment table
+-- INPUT: AFTER UPDATE ON `post_comment`
+-- OUTPUT: post.comment_count = post.comment_count + new.reply_count - old.reply_count
 DELIMITER $$
 CREATE TRIGGER `post_comment_AFTER_UPDATE` 
 AFTER UPDATE ON `post_comment` 
@@ -74,6 +105,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger decrease comment_count by 1 in post table whenever delete a rercord from post_comment table
+-- INPUT: AFTER DELETE ON `post_comment`
+-- OUTPUT: post.comment_count = post.comment_count - 1
 DELIMITER $$
 CREATE TRIGGER `post_comment_AFTER_DELETE` 
 AFTER DELETE ON `post_comment` 
@@ -83,6 +118,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger increase react_count by 1 in post table whenever insert a new rercord to post_user_reaction table
+-- INPUT: AFTER INSERT ON `post_user_reaction`
+-- OUTPUT: post.react_count = post.react_count + 1
 DELIMITER $$
 CREATE TRIGGER `post_user_reaction_AFTER_INSERT` 
 AFTER INSERT ON `post_user_reaction` 
@@ -92,6 +131,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger decrease react_count by 1 in post table whenever delete a rercord from post_user_reaction table
+-- INPUT: AFTER DELETE ON `post_user_reaction`
+-- OUTPUT: post.react_count = post.react_count - 1
 DELIMITER $$
 CREATE TRIGGER `post_user_reaction_AFTER_DELETE` 
 AFTER DELETE ON `post_user_reaction` 
@@ -101,6 +144,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger increase follower_count by 1 in topic table whenever insert a new rercord to topic_follower table
+-- INPUT: AFTER INSERT ON `topic_follower`
+-- OUTPUT: topic.follower_count = topic.follower_count + 1
 DELIMITER $$
 CREATE TRIGGER `topic_follower_AFTER_INSERT` 
 AFTER INSERT ON `topic_follower` 
@@ -110,6 +157,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger decrease follower_count by 1 in post table whenever delete a rercord from post_user_reaction table
+-- INPUT: AFTER DELETE ON `post_user_reaction`
+-- OUTPUT: post.follower_count = post.follower_count - 1
 DELIMITER $$
 CREATE TRIGGER `topic_follower_AFTER_DELETE` 
 AFTER DELETE ON `topic_follower` 
@@ -119,6 +170,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger increase follower_count by 1 in user table whenever insert a new rercord to user_follower table
+-- INPUT: AFTER INSERT ON `user_follower`
+-- OUTPUT: user.follower_count = user.follower_count + 1
 DELIMITER $$
 CREATE TRIGGER `user_follower_AFTER_INSERT` 
 AFTER INSERT ON `user_follower` 
@@ -128,6 +183,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger decrease follower_count by 1 in user table whenever delete a rercord from user_follower table
+-- INPUT: AFTER DELETE ON `user_follower`
+-- OUTPUT: user.follower_count = user.follower_count - 1
 DELIMITER $$
 CREATE TRIGGER `user_follower_AFTER_DELETE` 
 AFTER DELETE ON `user_follower` 
@@ -137,6 +196,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger check whether the user deleted is manager of any group
+-- INPUT: BEFORE DELETE ON `user`
+-- OUTPUT: error message if the user deleted is manager of (a) group(s)
 DELIMITER $$
 CREATE TRIGGER `user_BEFORE_DELETE` 
 BEFORE DELETE ON `user`
@@ -150,6 +213,10 @@ BEGIN
 END $$
 DELIMITER ;
 
+
+-- Trigger insert group manager as a member by inserting a new rercord to group_member table whenever create a new group
+-- INPUT: AFTER INSERT ON `group`
+-- OUTPUT: INSERT INTO `group_member` (member_id, group_id) VALUES (new.manager_id, new.group_id)
 DELIMITER $$
 CREATE TRIGGER `group_AFTER_INSERT` 
 AFTER INSERT ON `group` 
